35f0d8062270416dab91d6b46505d8392aa2a5a6
amanihmimida <amanihmimida@gmail.com>
2020-04-12 13:18:53 +0200

    Créaction et Developpement de l'interface TaskDescriptorActivity(Design)

---
+++ app/src/main/java/com/example/taskand/TaskDescriptorActivity.java
@@ -0,0 +1,14 @@
+package com.example.taskand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskDescriptorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_descriptor);
+    }
+}
