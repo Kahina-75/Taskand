5a23877da67fda6c1c4c25a669a92198ae4c972a
EL ABAOUI Asmae <ela.asmae1@gmail.com>
2020-04-14 13:32:41 +0000

    ajout task OK !

--- Taskand/app/src/main/java/com/example/taskand/MakeTaskActivity.java
+++ Taskand/app/src/main/java/com/example/taskand/MakeTaskActivity.java
@@ -3,6 +3,7 @@ package com.example.taskand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -25,6 +26,7 @@ import java.util.Calendar;
 public class MakeTaskActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String curDate;
+    private JSONObject jsonFile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,36 +38,8 @@ public class MakeTaskActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.bt1){
-            JSONObject obj = new JSONObject();
-            JSONObject person = new JSONObject();
-            JSONArray jarr = null;
-            JSONObject file = null;
-            String tmp =null;
-            try{
-                file = readFromJsonFile("TasksFile.json");
-                EditText name = (EditText)findViewById(R.id.edtName);
-                tmp = name.getText().toString();
-                EditText desc = (EditText)findViewById(R.id.edtDescription);
-                EditText edtPrio = (EditText)findViewById(R.id.edtPriority);
-                DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker1);
-                int day = datePicker.getDayOfMonth();
-                int month = datePicker.getMonth() + 1;
-                int year = datePicker.getYear();
-                JSONObject o = new JSONObject();
-                o.put(name.getText().toString(),name.getText().toString());
-                jarr = file.getJSONArray("array");
-                jarr.put(o);
-                /*person.put("TaskName", name.getText());
-                person.put("TaskDescritpion", desc.getText());
-                person.put("TaskPriority", edtPrio.getText());
-                person.put("TaskDate", day+"-" + month + "-"+year);
-                person.put("array",jarr);*/
-                file.put("TaskName" , name.getText().toString());
-                file.put("array",jarr);
-            }catch(Exception exception){}
-            Toast toast = Toast.makeText(getApplicationContext(), tmp, Toast.LENGTH_SHORT);
-            toast.show();
-            mCreateAndSaveFile("TasksFile.json",file.toString());
+            addTask();
+            //supprimer("TEst");
         }
     }
 
@@ -80,54 +54,21 @@ public class MakeTaskActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
-    public void mReadJsonData(String params) {
-        try {
-            File f = new File("/data/data/" + getPackageName() + "/" + params);
-            FileInputStream is = new FileInputStream(f);
-            int size = is.available();
-            String tmp = "No Data";
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            String mResponse = new String(buffer);
-            try{
-                JSONObject obj = new JSONObject(mResponse);
-                tmp = obj.getString("TaskName");
-
-            }catch (Exception e){}
-            Toast toast = Toast.makeText(getApplicationContext(), tmp, Toast.LENGTH_SHORT);
-            toast.show();
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-
     public  JSONObject readFromJsonFile(String fileName){
         ArrayList<String> result = new ArrayList<String>();
         JSONObject obj = null;
         try{
             File f = new File("/data/data/" + getPackageName() + "/" + fileName);
+            if(!f.exists()){
+                mCreateAndSaveFile("TasksFile.json","{'tasks':[]}");
+            }
             FileInputStream is = new FileInputStream(f);
             int size = is.available();
-            String tmp = "No Data";
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-
             String mResponse = new String(buffer);
             obj = new JSONObject(mResponse);
-            result.add(obj.getString("TaskName"));
-
-            //EditText name = (EditText)findViewById(R.id.edtName);
-            //name.setText(obj.getString("TaskName"));
-            JSONArray arr = obj.getJSONArray("array");
-            for(int i = 0; i < arr.length(); i++){
-                result.add(arr.getJSONObject(i).getString("t1"));
-            }
-            //result.add(arr.getJSONObject(0).getString("t1"));
         return  obj;
         }
         catch(Exception ex){
@@ -136,6 +77,48 @@ public class MakeTaskActivity extends AppCompatActivity implements View.OnClickL
         }
 
     }
+
+    public void addTask(){
+        JSONObject obj = new JSONObject();
+        JSONObject person = new JSONObject();
+        JSONArray jarr = null;
+        JSONObject file = null;
+        String tmp =null;
+        try{
+            file = readFromJsonFile("TasksFile.json");
+            //file = new JSONObject();
+            jarr = file.getJSONArray("tasks");
+            JSONObject obj1 = new JSONObject();
+
+            EditText name = (EditText)findViewById(R.id.edtName);
+            tmp = name.getText().toString();
+            EditText desc = (EditText)findViewById(R.id.edtDescription);
+            EditText edtPrio = (EditText)findViewById(R.id.edtPriority);
+            DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker1);
+            int day = datePicker.getDayOfMonth();
+            int month = datePicker.getMonth() + 1;
+            int year = datePicker.getYear();
+            obj1.put("TaskName",name.getText().toString());
+            obj1.put("TaskDesc",desc.getText().toString());
+            obj1.put("TaskDate",day+"-" + month + "-"+year);
+
+            JSONArray subTask = new JSONArray();
+            obj1.put("subTask",subTask);
+            jarr.put(obj1);
+
+            file.put("tasks",jarr);
+
+        }catch(Exception exception){}
+        Toast toast = Toast.makeText(getApplicationContext(), tmp, Toast.LENGTH_SHORT);
+        toast.show();
+        mCreateAndSaveFile("TasksFile.json",file.toString());
+    }
+
+    public void modifier(){
+
+    }
+
+
 
 
 }
