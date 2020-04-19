ddf4a166f727e9353823825d4c6dbaad90273085
EL ABAOUI Asmae <ela.asmae1@gmail.com>
2020-04-19 19:27:29 +0000

    Modification Back-end Ok ! EndTASK

--- Taskand/app/src/main/java/com/example/taskand/ModiFieldActivity.java
+++ Taskand/app/src/main/java/com/example/taskand/ModiFieldActivity.java
@@ -8,8 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ModiFieldActivity extends AppCompatActivity {
     Button addSon;
+    Button editButton;
     String nomTask = "";
     String descripTask = "";
     EditText name,descrip,pre;
@@ -49,5 +59,69 @@ public class ModiFieldActivity extends AppCompatActivity {
 
             }
         });
+
+        editButton = findViewById(R.id.modif);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modifier("TasksFile.json");
+            }
+        });
     }
+
+    public void modifier(String taskName){
+        JSONObject obj = new JSONObject();
+        JSONObject person = new JSONObject();
+        JSONArray jarr = null;
+        JSONObject file = null;
+        try{
+            file = readFromJsonFile("TasksFile.json");
+            //file = new JSONObject();
+            jarr = file.getJSONArray("tasks");
+
+            jarr.getJSONObject(position).put("TaskName",name.getText());
+            jarr.getJSONObject(position).put("TaskDesc",descrip.getText());
+            jarr.getJSONObject(position).put("TaskPre",pre.getText());
+
+            file.put("tasks",jarr);
+        }catch(Exception exception){}
+
+        mCreateAndSaveFile("TasksFile.json",file.toString());
+    }
+
+    public void mCreateAndSaveFile(String params, String mJsonResponse) {
+        try {
+            FileWriter file = new FileWriter("/data/data/" + getApplicationContext().getPackageName() + "/" + params);
+            file.write(mJsonResponse);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  JSONObject readFromJsonFile(String fileName){
+        ArrayList<String> result = new ArrayList<String>();
+        JSONObject obj = null;
+        try{
+            File f = new File("/data/data/" + getPackageName() + "/" + fileName);
+            if(!f.exists()){
+                mCreateAndSaveFile("TasksFile.json","{'tasks':[]}");
+            }
+            FileInputStream is = new FileInputStream(f);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String mResponse = new String(buffer);
+            obj = new JSONObject(mResponse);
+            return  obj;
+        }
+        catch(Exception ex){
+            System.out.println(ex.toString());
+            return obj;
+        }
+
+    }
+
 }
